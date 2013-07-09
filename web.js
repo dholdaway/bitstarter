76a81667725248326956e46658e9c6fd067751e7
Darren Holdaway <d.m.holdaway@googlemail.com>
2013-07-09 19:07:54 +0000

    rewrote 14

--- web.js
+++ web.js
@@ -1,23 +1,23 @@
 //#!/usr/bin/env node
-//var express = require('express');
+var express = require('express');
 
-//var app = express.createServer(express.logger());
+var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-//});
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
+});
 
-var fs = require('fs');
+//var fs = require('fs');
 
-var contents = fs.readFileSync("index.html", "utf8");
-var buffer = new Buffer('contents');
-buffer.toString('utf-8', 0, 12)
+//var contents = fs.readFileSync("index.html", "utf8");
+//var buffer = new Buffer('contents');
+//buffer.toString('utf-8', 0, 12)
 
-console.log(contents);
+//console.log(contents);
 
 
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
- // console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+ console.log("Listening on " + port);
+});
