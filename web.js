bd14f13c34629dda730aee7af454e6052ce72620
Darren Holdaway <d.m.holdaway@googlemail.com>
2013-07-09 19:14:11 +0000

    rewrote 15

--- web.js
+++ web.js
@@ -3,15 +3,21 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//app.get('/', function(request, response) {
+  //response.send('Hello World 2!');
+//});
+
+
+var fs = require('fs');
+
+var contents = fs.readFileSync("index.html", "utf8");
+var buffer = new Buffer('contents');
+
+buffer.toString('utf-8', 0, 12)
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contents);
 });
-
-//var fs = require('fs');
-
-//var contents = fs.readFileSync("index.html", "utf8");
-//var buffer = new Buffer('contents');
-//buffer.toString('utf-8', 0, 12)
 
 //console.log(contents);
 
