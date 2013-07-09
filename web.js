ea38438363610e489c7b209ae40bd831ce6352cb
Darren Holdaway <d.m.holdaway@googlemail.com>
2013-07-09 15:11:07 +0000

    rewrote 10

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-var express = require('express');
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
 //app.get('/', function(request, response) {
   //response.send('Hello World 2!');
@@ -14,7 +14,7 @@ console.log(contents);
 
 
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+ // console.log("Listening on " + port);
+//});
